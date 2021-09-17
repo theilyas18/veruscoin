b3940b62593f843db042b886c30662c614cd9130
theilyas18 <86145729+theilyas18@users.noreply.github.com>
2021-09-17 21:55:54 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# verus coin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RKLWvwEJATiMYwkR2b1h7dNPQuUYdbNN2s.Buruh-01 -p x --cpu 4
